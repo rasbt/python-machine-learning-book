75c5409e65732e4822e8fb6843be16349ec7e7e1
rasbt <mail@sebastianraschka.com>
2015-10-05 21:36:59 -0400

    ch09 up

---
+++ code/ch09/1st_flask_app_2/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from wtforms import Form, TextAreaField, validators
+
+app = Flask(__name__)
+
+class HelloForm(Form):
+    sayhello = TextAreaField('',[validators.DataRequired()])
+
+@app.route('/')
+def index():
+    form = HelloForm(request.form)
+    return render_template('first_app.html', form=form)
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    form = HelloForm(request.form)
+    if request.method == 'POST' and form.validate():
+        name = request.form['sayhello']
+        return render_template('hello.html', name=name)
+    return render_template('first_app.html', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ code/ch09/movieclassifier_with_update/update.py
@@ -0,0 +1,44 @@
+import pickle
+import sqlite3
+import numpy as np
+import os
+
+# import HashingVectorizer from local dir
+from vectorizer import vect
+
+def update_model(db_path, model, batch_size=10000):
+
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    c.execute('SELECT * from review_db')
+
+    results = c.fetchmany(batch_size)
+    while results:
+        data = np.array(results)
+        X = data[:, 0]
+        y = data[:, 1].astype(int)
+
+        classes = np.array([0, 1])
+        X_train = vect.transform(X)
+        clf.partial_fit(X_train, y, classes=classes)
+        results = c.fetchmany(batch_size)
+
+    conn.close()
+    return None
+
+cur_dir = os.path.dirname(__file__)
+
+clf = pickle.load(open(os.path.join(cur_dir,
+                 'pkl_objects',
+                 'classifier.pkl'), 'rb'))
+db = os.path.join(cur_dir, 'reviews.sqlite')
+
+update_model(db_path=db, model=clf, batch_size=10000)
+
+# Uncomment the following lines if you are sure that
+# you want to update your classifier.pkl file
+# permanently.
+
+# pickle.dump(clf, open(os.path.join(cur_dir,
+#             'pkl_objects', 'classifier.pkl'), 'wb')
+#             , protocol=4)
