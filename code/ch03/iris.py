1918aa9d5d5f48a7cf876092d15a2a0cc93f686c
liyiwei <git@liyiwei.org>
2016-05-22 15:43:14 -0700

    init iris play for sklearn

---
+++ code/ch03/iris.py
@@ -0,0 +1,94 @@
+import numpy as np
+from sklearn import datasets
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+import warnings
+
+def versiontuple(v):
+    return tuple(map(int, (v.split("."))))
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02, xlabel='', ylabel=''):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
+                    alpha=0.8, c=cmap(idx),
+                    marker=markers[idx], label=cl)
+
+    # highlight test samples
+    if test_idx:
+        # plot all samples
+        if not versiontuple(np.__version__) >= versiontuple('1.9.0'):
+            X_test, y_test = X[list(test_idx), :], y[list(test_idx)]
+            warnings.warn('Please update to NumPy 1.9.0 or newer')
+        else:
+            X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    alpha=1.0,
+                    linewidths=1,
+                    marker='o',
+                    s=55, label='test set')
+
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.legend(loc='upper left')
+
+    plt.tight_layout()
+    plt.show()
+
+# load iris data
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+print('Class labels:', np.unique(y))
+
+# split into training and test data
+from sklearn.cross_validation import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+# normalizing data
+from sklearn.preprocessing import StandardScaler
+
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+# train a perceptron
+from sklearn.linear_model import Perceptron
+
+ppn = Perceptron(n_iter=40, eta0=0.1, random_state=0)
+ppn.fit(X_train_std, y_train)
+
+y_pred = ppn.predict(X_test_std)
+print('Misclassified samples: %d' % (y_test != y_pred).sum())
+
+from sklearn.metrics import accuracy_score
+
+print('Accuracy: %.2f' % accuracy_score(y_test, y_pred))
+
+# visualize decision regions
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+plot_decision_regions(X=X_combined_std, y=y_combined, classifier=ppn, test_idx=range(105, 150), xlabel='petal length [standardized]', ylabel='petal width [standardized]')
