6c7ae371cea7d0628b1108a775526c0770606bc0
liyiwei <git@liyiwei.org>
2016-05-22 17:49:09 -0700

    regularization

--- code/ch03/iris.py
+++ code/ch03/iris.py
@@ -101,3 +101,24 @@ lr = LogisticRegression(C=1000.0, random_state=0)
 lr.fit(X_train_std, y_train)
 
 plot_decision_regions(X_combined_std, y_combined, classifier=lr, test_idx=test_idx, xlabel='petal length [standardized]', ylabel='petal width [standardized]')
+
+print(lr.predict_proba(X_test_std[0, :]))
+
+# regularization
+weights, params = [], []
+for c in np.arange(-5, 5):
+    lr = LogisticRegression(C=10**c, random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10**c)
+
+weights = np.array(weights)
+plt.plot(params, weights[:, 0],
+         label='petal length')
+plt.plot(params, weights[:, 1], linestyle='--',
+         label='petal width')
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+plt.legend(loc='upper left')
+plt.xscale('log')
+plt.show()
