a1e97f408dead398d81fc7a3c75ad1c45d7ec61d
liyiwei <git@liyiwei.org>
2016-05-23 18:03:58 -0700

    separate out plot_decision_regions

--- code/ch03/iris.py
+++ code/ch03/iris.py
@@ -1,58 +1,6 @@
 import numpy as np
 from sklearn import datasets
-from matplotlib.colors import ListedColormap
-import matplotlib.pyplot as plt
-import warnings
-
-def versiontuple(v):
-    return tuple(map(int, (v.split("."))))
-
-def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02, xlabel='', ylabel=''):
-
-    # setup marker generator and color map
-    markers = ('s', 'x', 'o', '^', 'v')
-    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
-    cmap = ListedColormap(colors[:len(np.unique(y))])
-
-    # plot the decision surface
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
-                           np.arange(x2_min, x2_max, resolution))
-    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
-    plt.xlim(xx1.min(), xx1.max())
-    plt.ylim(xx2.min(), xx2.max())
-
-    for idx, cl in enumerate(np.unique(y)):
-        plt.scatter(x=X[y == cl, 0], y=X[y == cl, 1],
-                    alpha=0.8, c=cmap(idx),
-                    marker=markers[idx], label=cl)
-
-    # highlight test samples
-    if test_idx:
-        # plot all samples
-        if not versiontuple(np.__version__) >= versiontuple('1.9.0'):
-            X_test, y_test = X[list(test_idx), :], y[list(test_idx)]
-            warnings.warn('Please update to NumPy 1.9.0 or newer')
-        else:
-            X_test, y_test = X[test_idx, :], y[test_idx]
-
-        plt.scatter(X_test[:, 0],
-                    X_test[:, 1],
-                    c='',
-                    alpha=1.0,
-                    linewidths=1,
-                    marker='o',
-                    s=55, label='test set')
-
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.legend(loc='upper left')
-
-    plt.tight_layout()
-    plt.show()
+from share import *
 
 # load iris data
 iris = datasets.load_iris()
---
+++ code/ch03/kernel_svm.py
@@ -0,0 +1,31 @@
+import numpy as np
+from share import *
+
+np.random.seed(0)
+X_xor = np.random.randn(200, 2)
+y_xor = np.logical_xor(X_xor[:, 0] > 0,
+                       X_xor[:, 1] > 0)
+y_xor = np.where(y_xor, 1, -1)
+
+plt.scatter(X_xor[y_xor == 1, 0],
+            X_xor[y_xor == 1, 1],
+            c='b', marker='x',
+            label='1')
+plt.scatter(X_xor[y_xor == -1, 0],
+            X_xor[y_xor == -1, 1],
+            c='r',
+            marker='s',
+            label='-1')
+
+plt.xlim([-3, 3])
+plt.ylim([-3, 3])
+plt.legend(loc='best')
+plt.tight_layout()
+plt.show()
+
+# svm
+from sklearn.svm import SVC
+
+svm = SVC(kernel='rbf', random_state=0, gamma=0.10, C=10.0)
+svm.fit(X_xor, y_xor)
+plot_decision_regions(X_xor, y_xor, classifier=svm)
