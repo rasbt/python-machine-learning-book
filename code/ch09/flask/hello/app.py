7493159d3b90da4acdcb106c17b404ebbfeeb779
liyiwei <git@liyiwei.org>
2016-06-18 11:32:28 -0700

    update

--- code/ch09/flask/hello/app.py
+++ code/ch09/flask/hello/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello world!'
 
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % username
+
 if __name__ == '__main__':
     app.run();
